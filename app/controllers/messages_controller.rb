36b429ed5a12708c8cf11556310e40080db5db03
ryotaoka <taokaryo1215@gmail.com>
2017-08-12 04:39:16 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update. :destloy]
+  
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -24,12 +25,9 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
-
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -40,9 +38,18 @@ class MessagesController < ApplicationController
   end
 
   def destroy
+    @message.destroy
 
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to messages_url
   end
 end
+
+private
+
+  def set_message
+    @message = Message.find (params[:id])
+  end
 
   def message_params
       params.require(:message).permit(:content)
